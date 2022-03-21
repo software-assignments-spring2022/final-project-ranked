a8e460e62fabb7041e1d91696a27dee73bb09bf5
Yoshiaki Kato <katoy01@nyu.edu>
2022-03-21 14:32:23 -0400

    stuff

--- front-end/src/Account.js
+++ front-end/src/Account.js
@@ -1,30 +1,50 @@
-import logo from './logo.svg';
-import './Account.css';
+import logo from "./logo.svg"
+import "./Account.css"
 
-const Account = props => {
-    return (
-        <main className='Account'>
-            <header className='Account-header'>header section</header>
-            <img className='Account-image' src={props.mockImgSource} alt='user profile image'></img>
-            <div className='Account-tabDiv'>
-                <div className='Account-infoTab'>Overview</div>
-                <div className='Account-adminTab'>Admin</div>
-            </div>
-            <div className='Account-infoDiv'>
-                <div className='Account-details'>
-                    <p> <b>Username:</b> {props.username}</p>
-                </div>
-                <div className='Account-details'>
-                    <p> <b>Email address:</b> {props.email}</p>
-                </div>
-                <div className='Account-details'>
-                    <p> <b>Country:</b> {props.country}</p>
-                </div>
-                <button className='Account-resetPwBtn' onClick={props.handleResetPwClick}>Reset Password</button>
-                <button className='Account-delAccBtn' onClick={props.handleDelAccClick}>Deactivate Account</button>
-            </div>
-        </main>
-    )
+const Account = (props) => {
+  return (
+    <main className="Account">
+      <header className="Account-header">header section</header>
+      <img
+        className="Account-image"
+        src={props.mockImgSource}
+        alt="user profile image"
+      ></img>
+      <div className="Account-tabDiv">
+        <div className="Account-infoTab">Overview</div>
+        <div className="Account-adminTab">Admin</div>
+      </div>
+      <div className="Account-infoDiv">
+        <div className="Account-details">
+          <p>
+            {" "}
+            <b>Username:</b> {props.username}
+          </p>
+        </div>
+        <div className="Account-details">
+          <p>
+            {" "}
+            <b>Email address:</b> {props.email}
+          </p>
+        </div>
+        <div className="Account-details">
+          <p>
+            {" "}
+            <b>Country:</b> {props.country}
+          </p>
+        </div>
+        <button
+          className="Account-resetPwBtn"
+          onClick={props.handleResetPwClick}
+        >
+          Reset Password
+        </button>
+        <button className="Account-delAccBtn" onClick={props.handleDelAccClick}>
+          Deactivate Account
+        </button>
+      </div>
+    </main>
+  )
 }
 
-export default Account;
+export default Account
--- front-end/src/Post.js
+++ front-end/src/Post.js
@@ -1,23 +1,21 @@
 import "./Post.css"
 import Tag from "./Tag"
 
-const Post = props => {
+const Post = (props) => {
   return (
     <div className="Post">
-        <h1> {props.post.title} </h1>
-        <section className="post-body">
-            <img alt="game picture" src="https://picsum.photos/200?page=animals" />
-            <div className="details">
-                <p>user: {props.post.user}</p>
-                <p>{props.post.body}</p>
-                <section className="tags">
-                    {props.post.tags && 
-                        props.post.tags.map(item => (
-                            <Tag tagName={item} />
-                    ))}
-                </section>
-            </div>
-        </section>
+      <h1> {props.post.title} </h1>
+      <section className="post-body">
+        <img alt="game picture" src="https://picsum.photos/200?page=animals" />
+        <div className="details">
+          <p>user: {props.post.user}</p>
+          <p>{props.post.body}</p>
+          <section className="tags">
+            {props.post.tags &&
+              props.post.tags.map((item) => <Tag tagName={item} />)}
+          </section>
+        </div>
+      </section>
     </div>
   )
 }
