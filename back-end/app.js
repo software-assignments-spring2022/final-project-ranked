e6d4c86fa4066a9fb966bc57513b0136f9492810
M2JT <m2jt998@gmail.com>
2022-03-30 23:44:09 -0400

    implement login feature (halfway through) from task #57

--- back-end/app.js
+++ back-end/app.js
@@ -3,16 +3,51 @@ const path = require("path")
 const morgan = require("morgan") // middleware for nice logging of incoming HTTP requests
 const multer = require("multer") // middleware to handle HTTP POST requests with file uploads
 const mongoose = require("mongoose") // library for MongoDB
+const fs = require("fs") // module to handle readfile or writefile
+const cors = require("cors") // middleware for enabling CORS (Cross-Origin Resource Sharing) requests.
 const app = express() // instantiate an Express object
 
-app.use(morgan("dev")) // use the morgan middleware to log all incoming http requests
 app.use(express.json()) // decode JSON-formatted incoming POST data
 app.use(express.urlencoded({ extended: true })) // decode url-encoded incoming POST data
 app.use("/static", express.static("public")) // make 'public' directory publicly readable with static content
+app.use(cors()) // allow cross-origin resource sharing
+app.use(morgan("dev")) // use the morgan middleware to log all incoming http requests
 
 // demo: route for HTTP GET requests to the root document
 app.get("/", (req, res) => {
     res.send("Welcome to Ranked!")
+})
+
+app.post("/login", (req, res) => {
+    const username = req.body.username.toLowerCase()
+    const password = req.body.password
+    
+    if(!username.trim() || !password){
+        return res.json({
+            missing: "Username or password missing!"
+        })   
+    }
+    else{
+        fs.readFile("./user.json", (err, data) => {
+            if(err){
+                console.log(err)
+            }
+            else{
+                const user = JSON.parse(data)
+                if(user.username != username){
+                    return res.json({
+                        notFound: "User not found!"
+                    }) 
+                }
+                else{
+                    return res.json({
+                        success: "Login success!",
+                        user: user
+                    }) 
+                }
+            }
+        })
+    }
 })
 
 // export the express app created to make it available to other modules
--- front-end/src/Login.js
+++ front-end/src/Login.js
@@ -1,10 +1,41 @@
-import { Link } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './Login.css';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './Login.css'
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+import axios from "axios"
 
 const Login = props => {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    const HandleLoginClick = (e) => {
+        e.preventDefault()
+        
+        axios
+            .post("http://localhost:4000/login", {
+              username: username,
+              password: password
+            })
+            .then(res => {
+                if(res.data.missing){
+                    alert(res.data.missing)
+                }
+                else if(res.data.notFound){
+                    alert(res.data.notFound)
+                }
+                else{
+                    window.location.href = "/account"
+                }
+            })
+            .catch(err => {
+              console.log(err)
+            })
+        setUsername('')
+        setPassword('')
+    } 
+
     return (
         <>
             <main className='Login'>
@@ -15,17 +46,25 @@ const Login = props => {
                     </div>
                 </div>
                 <div className='Login-loginDiv'>
-                    <Form className='Login-form'>
+                    <Form className='Login-form' onSubmit={HandleLoginClick}>
                         <div className='Login-usernameEmailForm'>
                             <Form.Label>Username / Email</Form.Label>
-                            <Form.Control placeholder="Enter username / email" />
+                            <Form.Control 
+                                placeholder="Enter username / email" 
+                                value={username} 
+                                onChange={e => setUsername(e.target.value)}
+                            />
                         </div>
                         <div className='Login-passwordForm'>
                             <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" />
+                            <Form.Control 
+                                type="password" 
+                                placeholder="Password"
+                                value={password} 
+                                onChange={e => setPassword(e.target.value)}
+                            />
                         </div>
-                        <Button className='Login-submitBtn' href="/account" 
-                        onClick={props.handleLoginClick}>Login</Button>
+                        <Button type='submit' className='Login-submitBtn' >Login</Button>
                     </Form>
                 </div>
             </main>
