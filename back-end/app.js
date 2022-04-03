516dcfbfbe4767da9781876288e91d4765a59d0a
Yoshiaki Kato <katoy01@nyu.edu>
2022-04-02 20:19:37 -0400

    fetch comments from backend

--- back-end/app.js
+++ back-end/app.js
@@ -1,11 +1,12 @@
 const express = require("express") // import and instantiate express
-const path = require("path") 
+const path = require("path")
 const morgan = require("morgan") // middleware for nice logging of incoming HTTP requests
 const multer = require("multer") // middleware to handle HTTP POST requests with file uploads
 const mongoose = require("mongoose") // library for MongoDB
 const cors = require("cors")
 const app = express() // instantiate an Express object
 const allPosts = require("./post.json")
+const allComments = require("./comment.json")
 
 app.use(morgan("dev")) // use the morgan middleware to log all incoming http requests
 app.use(express.json()) // decode JSON-formatted incoming POST data
@@ -15,60 +16,79 @@ app.use(cors())
 
 // demo: route for HTTP GET requests to the root document
 app.get("/", (req, res) => {
-    res.send("Welcome to Ranked!")
+  res.send("Welcome to Ranked!")
 })
 
 app.get("/posts", async (req, res) => {
   try {
     var home_posts = []
-    allPosts.map(item => home_posts = home_posts.concat(item.posts))
+    allPosts.map((item) => (home_posts = home_posts.concat(item.posts)))
     res.json({
-        home_posts: home_posts,
-        status: 'all good',
+      home_posts: home_posts,
+      status: "all good",
     })
-} catch (err) {
+  } catch (err) {
     console.error(err)
     res.status(400).json({
-        error: err,
-        status: 'failed to retrieve messages from the database',
+      error: err,
+      status: "failed to retrieve posts from the database",
     })
-}
+  }
 })
 
 app.get("/megathread/:gameId/posts", async (req, res) => {
   try {
-      const game_posts = allPosts.find( element => 
-          element.megathreadId == req.params.gameId).posts
-      res.json({
-          game_posts: game_posts,
-          status: 'all good',
-      })
-  } catch (err) {
-      console.error(err)
-      res.status(400).json({
-          error: err,
-          status: 'failed to retrieve messages from the database',
-      })
-  }
-})
-
-app.get("/megathread/:gameId/subthread/:postId/post", async (req,res) => {
-  try {
-    const sub_post = allPosts.find( element => 
-        element.megathreadId == req.params.gameId).posts.find( element =>
-          element.post_id == req.params.postId)
-        res.json({
-            sub_post: sub_post,
-            status: 'all good',
-        })
-        console.log(`${sub_post.postId}`)
+    const game_posts = allPosts.find(
+      (element) => element.megathreadId == req.params.gameId
+    ).posts
+    res.json({
+      game_posts: game_posts,
+      status: "all good",
+    })
   } catch (err) {
     console.error(err)
     res.status(400).json({
-        error: err,
-        status: 'failed to retrieve messages from the database',
+      error: err,
+      status: "failed to retrieve posts from the database",
     })
-}
+  }
+})
+
+app.get("/megathread/:gameId/subthread/:postId/post", async (req, res) => {
+  try {
+    const sub_post = allPosts
+      .find((element) => element.megathreadId == req.params.gameId)
+      .posts.find((element) => element.post_id == req.params.postId)
+    res.json({
+      sub_post: sub_post,
+      status: "all good",
+    })
+  } catch (err) {
+    console.error(err)
+    res.status(400).json({
+      error: err,
+      status: "failed to retrieve post from the database",
+    })
+  }
+})
+
+app.get("/megathread/:gameId/subthread/:postId/comments", async (req, res) => {
+  try {
+    // const comments = allPosts
+    //   .find((element) => element.megathreadId == req.params.gameId)
+    //   .posts.find((element) => element.post_id == req.params.postId).comments
+    const comments = allComments.find((element) => (element.gameId == req.params.gameId && element.postId == req.params.postId)).comments
+    res.json({
+      comments: comments,
+      status: "all good",
+    })
+  } catch (err) {
+    console.error(err)
+    res.status(400).json({
+      error: err,
+      status: "failed to retrieve post comments from the database",
+    })
+  }
 })
 
 // export the express app created to make it available to other modules
--- front-end/src/CommentSection.js
+++ front-end/src/CommentSection.js
@@ -6,7 +6,27 @@ import Comment from "./Comment"
 import "./CommentSection.css"
 
 const CommentSection = props => {
-  const [comment, setComment] = useState("") 
+  const [data, setData] = useState([])
+  const [comment, setComment] = useState({}) 
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+    // fetch some mock data about animals for sale
+    // the id of the animal that was clicked on is passed as a part of the match field of the props
+    console.log(`fetching comments for gameId=${props.gameId} id=${props.postId}...`)
+    axios(`${process.env.REACT_APP_SERVER_HOSTNAME}/megathread/${props.gameId}/subthread/${props.postId}/comments`)
+        .then(response => {
+            // extract the data from the server response
+            setData(response.data.comments)
+        })
+        .catch(err => {
+            // Mockaroo, which we're using for our Mock API, only allows 200 requests per day on the free plan
+            console.log(`Sorry, buster.  No more requests allowed today!`)
+            console.error(err) // the server returned an error... probably too many requests... until we pay!
+
+            setData([])
+        })
+}, [])
 
   const handleSubmit = (e) => {
     e.preventDefault()  // prevent the default browser form submission stuff
