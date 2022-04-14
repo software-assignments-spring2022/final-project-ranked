9a6a4fe1bfa81aadb3b70fe4a120fe3e09c725d6
igdaniel-1 <igracedaniel@gmail.com>
2022-04-14 18:31:20 -0400

    modified List.js to include axios pull from back-end to gather posts

--- front-end/src/Components/List.js
+++ front-end/src/Components/List.js
@@ -1,19 +1,23 @@
-import { React, useState } from 'react'
+import { React, useState, useEffect } from 'react'
 // pick up tags.json data
-// import data from "./ListData.json"
+import data from "./ListData.json"
+import axios from "axios"
+
+// demo
+import backupData from "./../mock-backupPosts.json"
 
 // this brings in all the tags id's
 // e.g. filters thru all of the tag titles
 // import data from "../../../back-end/tags.json"
 
 const List = props => {
-    // create a new array by filtering the original array
+    // create setData to hold post array[]
+    const [data, setData] = useState([]) 
+
     // filteredData --> element.id
     // this will return the "game-name" of each tag
-
-    // replace 'data' with the 
     const filteredData = data.filter((el) => {
-        //if no input the return the original
+        // if no input the return the original
         // in order to hide the suggested tags before any text appears, 
             // i may !== the conditional
             // on Change
@@ -33,20 +37,21 @@ const List = props => {
     useEffect(() => {
         // fetch some mock data about animals for sasle
         // the id of the animal that was clicked on is passed as a part of the match field of the props
-        console.log(`fetching post id=${id}...`)
+        // console.log(`fetching post id=${id}...`)
         axios(`${process.env.REACT_APP_SERVER_HOSTNAME}/search`)        // empty file for .get() requests
             .then(response => {
                 // extract the data from the server response
-                setData(response.data[id])      // i changed the param to 'id'
+                setData(response.data)      // response.data = all posts
             })
             .catch(err => {
                 // Mockaroo, which we're using for our Mock API, only allows 200 requests per day on the free plan
                 console.log(`Sorry, buster.  No more requests allowed today!`)
                 console.error(err) // the server returned an error... probably too many requests... until we pay!
 
-                setData(backupData[id])
+                // using the demo data from the mock posts 
+                setData(backupData.data)
             })
-    }, [id])
+    })
 
     return (
         <ul>
@@ -56,6 +61,7 @@ const List = props => {
 
                 // original code
                 // this returns the game-name,posts
+                // if item.id in 
                 <li key={item.id}>{item.posts}</li>
             ))}
         </ul>
